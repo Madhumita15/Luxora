c75760c7b65c7561cf610c93212adb60b9be77b9
Madhumita Das <madhumita011das@gmail.com>
2025-07-19 21:02:37 +0530

    modify Carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Features from '../components/Features';
-import Carousel from '../components/carousel';
+import Carousel from '../components/Carousel';
+
 
 function Home() {
     return ( 
